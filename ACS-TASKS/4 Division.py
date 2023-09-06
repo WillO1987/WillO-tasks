ecdd7fa1b28e6cde5ec5f65d2f95862c2b4411cb
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-09-06 10:45:58 +0100

    start of task 5
    start of task 5

--- ACS-TASKS/4 Division.py
+++ ACS-TASKS/4 Division.py
@@ -1,6 +1,6 @@
 from math import *
-first_int = int(input("Enter an integer: "))
-divisor = int(input ("Enter a divisor: "))
-whole_num = first_int / divisor
-remainder_num = first_int // divisor
-print(whole_num , "remainder" , remainder_num )
+first_int = int(input("Enter an integer: ")) #asks user to input
+divisor = int(input ("Enter a divisor: ")) # """"""
+whole_num = first_int / divisor # whole division
+remainder_num = first_int // divisor #remainder revision
+print(whole_num , "remainder" , remainder_num ) # prints it out
---
+++ ACS-TASKS/5 Hundereds,tens and units.py
@@ -0,0 +1,5 @@
+from math import *
+first_int = int(input("Enter an integer between 100 and 199: ")) #asks user to input
+if first_int >= 100 and first_int <= 199:
+    print()
+#endif
