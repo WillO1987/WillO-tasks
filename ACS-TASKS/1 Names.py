243637d3a34a2c608841c0542d4c5686c96d59fa
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-09-06 10:27:03 +0100

    first 2 tasks

---
+++ ACS-TASKS/1 Names.py
@@ -0,0 +1,3 @@
+first_Name = input("Enter your first name: ") # takes the input of first name
+second_Name = input("Enter your surname: ") # takes the input of second name
+print("Your full name is" , first_Name , second_Name) # this prints out the full name
---
+++ ACS-TASKS/2 Password.py
@@ -0,0 +1,7 @@
+password = input("Input your password") #asks user to enter password and stores it in passwords
+if len(password) > 6: # calculates the length of the input of charactures and compares it to 6
+    print("Password valid")
+
+else: # else if its smaller then 6 
+    print("Password invald")
+#endif
