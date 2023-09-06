03b273950048d74d800f2758e987512a63e5ac53
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-09-06 10:40:40 +0100

    task 4

--- ACS-TASKS/3 Options.py
+++ ACS-TASKS/3 Options.py
@@ -1,6 +1,6 @@
 running = True #creates booleon var
 while running == True: #while loop
-    option = input(int("Enter your an number between 1 and 3")) # asks user to pick between 1 and 3 
+    option = int(input("Enter your an number between 1 and 3")) # asks user to pick between 1 and 3 
     if option >=1 and option <= 3: #if statement making sure between 1 and 3
         print("You have selcted option number" , option) # if it is prints choice
         running = False #ends while loop
---
+++ ACS-TASKS/4 Division.py
@@ -0,0 +1,6 @@
+from math import *
+first_int = int(input("Enter an integer: "))
+divisor = int(input ("Enter a divisor: "))
+whole_num = first_int / divisor
+remainder_num = first_int // divisor
+print(whole_num , "remainder" , remainder_num )
