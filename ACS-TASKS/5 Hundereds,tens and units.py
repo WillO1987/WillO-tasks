bd06b896ed6b8a1473bd1e9c36cd81d1a4edab5d
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-09-10 20:59:28 +0100

    task update

--- ACS-TASKS/5 Hundereds,tens and units.py
+++ ACS-TASKS/5 Hundereds,tens and units.py
@@ -1,5 +1,18 @@
 from math import *
-first_int = int(input("Enter an integer between 100 and 199: ")) #asks user to input
-if first_int >= 100 and first_int <= 199:
-    print()
-#endif
+valid = False
+while valid == False:
+    first_int = int(input("Enter an integer between 100 and 199: ")) #asks user to input
+
+    if first_int >= 100 and first_int <= 199: # checks if input is within range 
+        valid = True
+        
+    #endif
+    else:
+        print("invalid input")
+        
+#endwhile
+
+hundreds = first_int // 100
+tenths = first_int // 10 # whole div
+remainder = first_int % 10  # remainder
+print( hundreds + "hundreds" + tenths + "tenths and" + remainder + "Remainder") # prints it out
---
+++ ACS-TASKS/6 Numbersequence.py
@@ -0,0 +1,5 @@
+from math import *
+
+for i in range(1,11): # loops 10 times
+    print(i) # prints i each time
+#end for
