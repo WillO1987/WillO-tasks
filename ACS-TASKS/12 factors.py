f5e5a286597cf881933efb057258c63d7ab8869b
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-09-12 15:29:36 +0100

    Create 12 factors.py

---
+++ ACS-TASKS/12 factors.py
@@ -0,0 +1,10 @@
+first_int = int(input("Enter an integer: ")) #asks user to input
+
+factorlist = [] # empty array
+
+for i in range(2 , first_int + 1): # loops round from 2 to the input unmber 
+    if first_int % i == 0: # checks for no rmaineder from division 
+        factorlist.append() # if there is no remainder then it is a factor of the number
+
+
+print ("the factors are" , factorlist) # prints it out 
