a0473e5ed7a367ae858f5faaa4029cf39d2c2cb9
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-09-13 09:53:47 +0100

    Update 12 factors.py

--- ACS-TASKS/12 factors.py
+++ ACS-TASKS/12 factors.py
@@ -4,7 +4,7 @@ factorlist = [] # empty array
 
 for i in range(2 , first_int + 1): # loops round from 2 to the input unmber 
     if first_int % i == 0: # checks for no rmaineder from division 
-        factorlist.append() # if there is no remainder then it is a factor of the number
+        factorlist.append(first_int) # if there is no remainder then it is a factor of the number
 
 
 print ("the factors are" , factorlist) # prints it out 
