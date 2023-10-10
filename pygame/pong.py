3eff9530f19508eb2b4e045fb2a438a7a8ab1a1c
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-10-10 15:38:20 +0100

    Update pong.py

--- pygame/pong.py
+++ pygame/pong.py
@@ -32,6 +32,7 @@ counter = 0
 screenfill = RED
 
 y_val_2 = 200
+y_val_3 = 200
 x_val_2 = 5
 lives = 5
 text_font = pygame.font.SysFont(None, 30)
@@ -77,10 +78,14 @@ while not done:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        y_val_2 -= 5
+        y_val_3 -= 5
     if keys[pygame.K_DOWN]:
-        y_val_2 += 5
-
+        y_val_3 += 5
+    
+    if keys[pygame.K_s]:
+        y_val_2 +=5
+    if keys[pygame.K_w]:
+        y_val_2 -=5
 
     y_val = y_val+ y_offset
     x_val = x_val + x_offset
@@ -91,13 +96,20 @@ while not done:
         y_offset = y_offset*-1 
         
     
-    elif x_val > 695:
+    elif x_val > 670 and y_val >= y_val_3 and y_val <= y_val_3 + 100:
         x_offset = x_offset*-1 
-    
+        randomint = random.randint(1,4)
+        screenfill = randombackground(randomint)
     elif x_val < 20 and y_val >= y_val_2 and y_val <= y_val_2 + 100:#collisionwith paddel
         x_offset = x_offset*-1 
         randomint = random.randint(1,4)
         screenfill = randombackground(randomint)
+    if x_val > 700 and y_val <=y_val_3 + 100:
+             
+        x_val = 350
+        y_val = 250
+        
+        lives -= 1
         
     if x_val < 0 and y_val >= y_val_2+100:
        
@@ -113,24 +125,27 @@ while not done:
         
         lives -= 1    
     
-    if lives == 0:
+    if lives == -1:
         end = "GAME OVER"
         endmessage = font.render(end, True, WHITE)
         screen.blit(endmessage, [271,103])
+        screen.blit(lives_count, [271,130])
         pygame.display.flip() # clears the screen 
         time.sleep(1.5)
         done = True
     
 
     #end if
-
-    pygame.draw.circle(screen, WHITE, [x_val, y_val] , 10)
+    pygame.draw.rect(screen, WHITE, (x_val, y_val, 15 , 15))
+   # pygame.draw.circle(screen, WHITE, [x_val, y_val] , 10) 
 
     pygame.draw.rect(screen, WHITE, (5, y_val_2, 15, 100))
+
+    pygame.draw.rect(screen, WHITE, (680, y_val_3, 15, 100))
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
  
-    # --- Limit to 60 frames per second
+    # --- Limit to 60 frames per secon
     clock.tick(60)
 #endwhile
 pygame.quit()
