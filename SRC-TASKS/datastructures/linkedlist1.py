f72c6617901459d525185ab6e64fd79b1a4588d8
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-11-17 11:28:17 +0000

    wprk

--- SRC-TASKS/datastructures/linkedlist1.py
+++ SRC-TASKS/datastructures/linkedlist1.py
@@ -6,17 +6,27 @@ class Node:
 
     def __repr__(self) -> str:
         return " |Data: "  + self.data + "   Ptr: " + str(self.pointer) + "| "
+    
 
+    def orderOutput(self):
+        print(self)
 #end node record
 
 linked_list_data = []
-linked_list_data.append(Node("Empty" , 1))
-linked_list_data.append(Node("Empty" , 2))
-linked_list_data.append(Node("Empty" , 3))
+linked_list_data.append(Node("Lucas" , 2))
+linked_list_data.append(Node("Ayaan" , 0))
+linked_list_data.append(Node("Suren" , -1))
 linked_list_data.append(Node("Empty" , 4))
 linked_list_data.append(Node("Empty" , -1))
 
-start_pointer = -1
+start_pointer = 1
+Next_Free = 3
 
-Next_Free = 0
-print(linked_list_data)
+def printOrder(Mylinkedlist):
+    global start_pointer
+    Current_pointer = start_pointer
+    while Current_pointer != -1:
+        print(Mylinkedlist[Current_pointer].data)
+        Current_pointer = Mylinkedlist[Current_pointer].pointer
+    #next current_pointer
+printOrder(linked_list_data)
---
+++ SRC-TASKS/datastructures/worksheet3T3.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, name , pointer):
+        self.data = name
+        self.pointer = pointer
+    #end constructor
+
+    def __repr__(self) -> str:
+        return " |Data: "  + self.name + "   Ptr: " + str(self.pointer) + "| "
+    
+
+    def orderOutput(self):
+        print(self)
+#end node record
+
+myList = [None for _ in range(50)]
+
+for index in range(49):
+    myList[index].pointer = index + 1
+#next index
+myList[49].pointer = None
+
+
+start_pointer = 1
+nextfree = 3
+
+def printOrder(Mylinkedlist):
+    global start_pointer
+    Current_pointer = start_pointer
+    while Current_pointer != -1:
+        print(Mylinkedlist[Current_pointer].name)
+        Current_pointer = Mylinkedlist[Current_pointer].pointer
+    #next current_pointer
+#printOrder(linked_list_data)
+
+
+def AddItem(newItem):
+    #check if list is full and if so, print error message
+    if nextfree == None:
+        print("Error")
+    else:
+        newName = myList[nextfree].name
+        if start == None:
+            temp = myList[nextfree].pointer       #save pointer
+            myList[nextfree].pointer = None
+            start = nextfree
+            nextfree = temp
+        else:
+            p = start
+            if newName < myList[p].name:  
+                start  = myList[nextfree].pointer
+                start = nextfree
+            else:   
+                placeFound = False    #general case
+                while myList[p].pointer != None and placeFound == False:
+                    #peek ahead
+                    if newName >= myList[myList[p].pointer].name:
+                        p = myList[p].pointer
+                    else:
+                        placefound = True
+                    
+                
+                temp = nextfree
+                nextfree = Node[nextfree].pointer
+                Node[temp].pointer = Node[p].pointer
+                Node[p].pointer = temp
+            
+        
+    
