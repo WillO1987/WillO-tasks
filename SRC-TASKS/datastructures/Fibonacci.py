fb96f3f992fc4322b33bf0a77dc60653789b4144
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-12-08 16:01:47 +0000

    Create Fibonacci.py

---
+++ SRC-TASKS/datastructures/Fibonacci.py
@@ -0,0 +1,24 @@
+import time
+start_time = time.time()
+time.sleep(3) #so that we can see how logng the code actually takes 
+def Fibonacci(n):
+    fibNumbers = [0,1]
+    for i in range(2,n+1):
+        fibNumbers.append(fibNumbers[i-1] + fibNumbers[i-2])
+    #next i
+    print(fibNumbers)
+   
+
+Fibonacci(2)
+
+print ("--- %s seconds ---" % (time.time () - start_time))
+
+# def fibbonacci(n): 
+#     if n== 0:
+#         return 0
+#     if n == 1:
+#         return 1
+#     return fibbonacci(n-1) + fibbonacci(n-2)
+# #endfunc
+# fibbonacci(2)
+# print ("--- %s seconds ---" % (time.time () - start_time))
