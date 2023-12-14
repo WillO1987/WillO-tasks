b4b9b9bcf71bfabf45704ec376ca78bc1d5dc2c2
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2023-12-14 11:33:26 +0000

    update

--- SRC-TASKS/datastructures/binarysearch.py
+++ SRC-TASKS/datastructures/binarysearch.py
@@ -1,3 +1,5 @@
+
+#<---NORMAL VERSION-->
 # def BinarySearch(aList, itemSought):
 #     found = False
 #     index = -1
@@ -17,32 +19,35 @@
 #     return index 
 
 
-def binarySearch(array, x, first, last):
+
+#Reccursive version
+
+def binarySearch(array, item, first, last):
 
     if last >= first:
 
         mid = first + (last - first)//2
 
         # If found at mid, then return it
-        if array[mid] == x:
+        if array[mid] == item:
             return mid
 
         # Search the left half
-        elif array[mid] > x:
-            return binarySearch(array, x, first, mid-1)
+        elif array[mid] > item:
+            return binarySearch(array, item, first, mid-1)
 
         # Search the right half
         else:
-            return binarySearch(array, x, mid + 1, last)
+            return binarySearch(array, item, mid + 1, last)
 
     else:
         return -1
 
 
 array = [3, 4, 5, 6, 7, 8, 9]
-x = 4
+item = 5
 
-result = binarySearch(array, x, 0, len(array)-1)
+result = binarySearch(array, item, 0, len(array)-1)
 
 if result != -1:
     print("item is present at  " + str(result))
